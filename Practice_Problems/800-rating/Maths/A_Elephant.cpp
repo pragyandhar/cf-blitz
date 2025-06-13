83af5ffe43884842fd4021d8f1bc237fb842c793
Pragyan <142593392+pragyandhar@users.noreply.github.com>
2025-06-13 13:01:52 +0530

    CF Question Solved ✅

---
+++ Practice_Problems/800-rating/Maths/A_Elephant.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <numeric>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+void solve() {
+    // Solution of the Question
+    // Input
+    int x;
+    cin >> x;
+    // Logic
+    int steps = (x + 4) / 5;
+    cout << steps << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // int t;
+    // cin >> t;
+    // while (t--) {
+    solve();
+    // }
+
+    return 0;
+}
